156bddf5396f1d0cfe802454fee0646ce07c4710
Anil7Patil <patilsanil7@gmail.com>
2024-01-04 18:43:04 +0530

    Eureka server done

--- Administrator-Service/src/main/java/com/micro/AdministratorService/AdministratorServiceApplication.java
+++ Administrator-Service/src/main/java/com/micro/AdministratorService/AdministratorServiceApplication.java
@@ -2,8 +2,10 @@ package com.micro.AdministratorService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdministratorServiceApplication {
 
 	public static void main(String[] args) {
